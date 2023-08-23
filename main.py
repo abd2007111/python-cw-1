d3f8b08ad7d01e608ca6acb1a2a695ed5600e174
abd2007111 <142683500+abd2007111@users.noreply.github.com>
2023-08-23 20:06:38 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
 # Change the following print statement so that it uses the addition operator + and prints out 8
-
+name=('abdulrahman')
+print(name)
+print('4+4')
+print('6/3')
+print('3*2')
+length=(5)
+width=(5)
+area=length+width
+print(f'the result is{area}')
+print(f'if the length of the rectangle is {length}, and width {width},than the area is {area} ')
 # Write a print statement that uses the division operator / 
 
 # Add a print statement that uses multiplication *
